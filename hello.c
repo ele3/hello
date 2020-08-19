6c36b026964bfd96b2fa2f603c01ff8167a52e2d
Eric Le <ele3@huskers.unl.edu>
2020-08-19 00:45:42 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Eric Le
+ * Date: August 18 2020
+ * 
+ * A simple hello world program
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    
+    printf("Hello world!\n");
+    
+    return 0;
+    
+}
