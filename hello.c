85ebecb64fdc2c015d07654ae146c653255c1edb
Eric Le <ele3@huskers.unl.edu>
2020-08-19 01:02:04 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,16 +1,17 @@
 /**
  * Author: Eric Le
  * Date: August 18 2020
- * 
+ *
  * A simple hello world program
  */
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    
-    printf("Hello world!\n");
-    
+
+    printf("Eric Le!\n");
+    printf("Computer Science\n");
+
     return 0;
-    
+
 }
